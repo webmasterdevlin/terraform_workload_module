ec1374ab302ee97be0fa65db6e58ff3426f93875
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-05 18:25:11 +0200

    update

--- workload/variables.tf
+++ workload/variables.tf
@@ -1,22 +1,4 @@
 
-## Terraform Workspace
-variable "subscription_id" {
-  description = "This is the subscription id. Then TF_VAR_subscription_id in environment variables of Terraform Workspace."
-  type        = string
-}
-variable "client_id" {
-  description = "This is the appId. Then TF_VAR_client_id in environment variables of Terraform Workspace."
-  type        = string
-}
-variable "client_secret" {
-  description = "This is the password. Then TF_VAR_client_secret in environment variables of Terraform Workspace."
-  type        = string
-}
-variable "tenant_id" {
-  description = "This is the tenant. Then TF_VAR_tenant_id in environment variables of Terraform Workspace."
-  type        = string
-}
-
 variable "workload_rg_name" {
   type = string
 }
